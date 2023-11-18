41344554f8c3a1e57e200695b227864758b020da
Piyakker <ericlien86@gmail.com>
2023-11-18 11:44:39 +0800

    Add Category Seed Data

--- BookStoreWeb/Data/ApplicationDbContext.cs
+++ BookStoreWeb/Data/ApplicationDbContext.cs
@@ -11,5 +11,14 @@ namespace BookStoreWeb.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Adventure", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Romance", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Horror", DisplayOrder = 3 }
+                );
+        }
     }
 }
