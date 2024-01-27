03680fb3cb352cb733862f013e30940ccdfc9dde
Piyakker <ericlien86@gmail.com>
2024-01-27 11:27:25 +0800

    Add ShoppingCart Functionality

--- BookStore.DataAccess/Repository/UnitOfWork.cs
+++ BookStore.DataAccess/Repository/UnitOfWork.cs
@@ -16,6 +16,8 @@ namespace BookStore.DataAccess.Repository
         public ICompanyRepository Company {  get; private set; }
         public IShoppingCartRepository ShoppingCart {  get; private set; }
         public IApplicationUserRepository ApplicationUser {  get; private set; }
+        public IOrderHeaderRepository OrderHeader { get; }
+        public IOrderDetailRepository OrderDetail { get; }
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
@@ -24,6 +26,8 @@ namespace BookStore.DataAccess.Repository
             Company = new CompanyRepository(_db);
             ShoppingCart = new ShoppingCartRepository(_db);
             ApplicationUser = new ApplicationUserRepository(_db);
+            OrderHeader = new OrderHeaderRepository(_db);
+            OrderDetail = new OrderDetailRepository(_db);
         }
 
         public void Save()
